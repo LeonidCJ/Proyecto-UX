3677aacc7640380f743582302b1061ee2ba4e04b
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-04-10 17:33:43 -0500

    feat:update dashvoard

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,43 +1,32 @@
-import { CardCita, CardNotification, CardAccess } from "../components/index.js";
+import {
+  CardCita,
+  ListAccess,
+  ListCitas,
+  ListNotification,
+} from "../components/index.js";
 
 export function Dashboard() {
   return `
     <section class="flex flex-col w-full gap-6 py-6 pr-12 pl-3">
       <header class="flex gap-2 items-center ">
         <div class="size-12 rounded-full bg-gray-400"></div>
-        <span class="text-2xl font-semibold"> Giovanni el kchudo del Año</span>
+        <span class="text-2xl font-semibold"> Diego Alberto Salazar Garcia</span>
       </header>
       
-      <main class="w-full">
+      <main class="w-full h-full flex flex-col gap-6 justify-between">
         <div class="flex justify-between">
           <div>
             <h3 class="text-2xl font-bold">Citas del dia jueves 10 de Abril del 2025</h3>
-            <section>
-              ${CardCita()}
-              ${CardCita()}
-              ${CardCita()}
-              ${CardCita()}
-              ${CardCita()}
-              ${CardCita()}
-              ${CardCita()}
-            </section>
+            ${ListCitas()}
           </div>
-          <aside class="flex flex-col gap-3">
+          <aside class="flex flex-col gap-3 border-l-2 border-black pl-4">
             <h3 class="text-2xl font-bold">Notificaciones</h3>
-            <section class="flex flex-col gap-2">
-              ${CardNotification()}
-              ${CardNotification()}
-              ${CardNotification()}
-            </section>
+            ${ListNotification()}
           </aside>
         </div>
-        <div class="flex flex-col gap-4">
+        <div class="flex flex-col gap-4 mb-10">
           <h3 class="text-2xl font-bold">Accesos rápidos</h3>
-          <div class="flex gap-8">
-            ${CardAccess()}
-            ${CardAccess()}
-            ${CardAccess()}
-          </div>
+            ${ListAccess()}
         </div>
       </main>
     </section>
