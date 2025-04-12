57ae8251931fe0aa8a5dee454dabf0c31df15d29
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-04-12 10:33:58 -0500

    feat: add router

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
-import { Navbar } from "./components/index.js";
-import { Dashboard } from "./pages/index.js";
+import { Navbar } from "./components/UI/index.js";
+import {
+  Dashboard,
+  Pacientes,
+  CitasMedicas,
+  Recetas,
+  NotFound,
+} from "./pages/index.js";
 
-document.getElementById("root").innerHTML = `
-  ${Navbar()}
-  ${Dashboard()}
-`;
+function navigate() {
+  const root = document.getElementById("root");
+
+  const page = window.location.hash || "#dashboard";
+
+  root.innerHTML = `
+    ${Navbar()}
+    ${getPageContent(page)}
+  `;
+}
+
+function getPageContent(page) {
+  switch (page) {
+    case "#dashboard":
+      return Dashboard();
+    case "#pacientes":
+      return Pacientes();
+    case "#citas-medicas":
+      return CitasMedicas();
+    case "#recetas":
+      return Recetas();
+    default:
+      return NotFound();
+  }
+}
+
+window.addEventListener("hashchange", navigate);
+
+navigate();
